878b4eddb7b5713fa21f44bf53ee7174f4fe28c6
Jidetireni <jtirenipraise@gmail.com>
2026-02-18 15:18:26 +0100

    style: reformat return statement in `New` function for better readability

--- pkg/zookeeper/zookeeper.go
+++ pkg/zookeeper/zookeeper.go
@@ -16,11 +16,15 @@ type Zookeeper struct {
 
 func New(config *config.Config) (*Zookeeper, error) {
 	server := fmt.Sprintf("%s:%s", config.ZooKeeperConfig.Host, config.ZooKeeperConfig.Port)
+
 	conn, _, err := zk.Connect([]string{server}, time.Second)
 	if err != nil {
 		return nil, fmt.Errorf("zookeeper: failed to connect to %s: %w", server, err)
 	}
-	return &Zookeeper{conn: conn}, nil
+
+	return &Zookeeper{
+		conn: conn,
+	}, nil
 }
 
 func (z *Zookeeper) GetNextRange(path string, blockSize int64) (int64, int64, error) {
